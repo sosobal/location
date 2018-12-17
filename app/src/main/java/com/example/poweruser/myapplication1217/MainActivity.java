eec222c5b8dcc98ad9f6aff75baaf2b613436f23
sosobal <sosobal1024@naver.com>
2018-12-17 15:38:28 +0900

    first commit

--- app/src/main/java/com/example/poweruser/myapplication1217/MainActivity.java
+++ app/src/main/java/com/example/poweruser/myapplication1217/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.poweruser.myapplication1217;
 
 import android.content.Intent;
 import android.location.Location;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,6 +20,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), LocationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btInfor = (Button) findViewById(R.id.btInfor);
+        btInfor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.tashu.or.kr/userStationAction.do?process=stationTotalList"));
                 startActivity(intent);
             }
         });
